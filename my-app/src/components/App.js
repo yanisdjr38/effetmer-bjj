f07bbc09467f8980cceda22661584809b4238484
Djouahra Yanis <yanis.djouahra@epitech.eu>
2025-03-13 18:11:58 +0100

    creat mysessions and mytechniques components

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,11 +1,13 @@
 import Banner from "./Banner.js";
 import MySessions from "./MySessions.js";
+import MyTechniques from "./MyTechniques.js";
 
 function App() {
   return (
     <div>
       <Banner />
       <MySessions />
+      <MyTechniques />
     </div>
   );
 }
---
+++ my-app/src/components/MyTechniques.js
@@ -0,0 +1,22 @@
+import "../styles/MyTechniques.css";
+
+function MyTechniques() {
+  return (
+    <section className="mytechniques">
+      <h3>My Techniques </h3>
+      <div className="techniquescard">
+        <h4>Triangle</h4>
+        <p>
+          Pour exécuter un triangle en JJB : Contrôle : Depuis la garde, passe
+          une jambe par-dessus l’épaule de ton adversaire en gardant un de ses
+          bras à l’intérieur. Ajustement : Croise ta cheville derrière ton genou
+          opposé et tire sa tête vers le bas. Verrouillage : Ajuste l’angle en
+          te décalant de côté, serre les jambes et pousse les hanches pour
+          finaliser l’étranglement.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default MyTechniques;
