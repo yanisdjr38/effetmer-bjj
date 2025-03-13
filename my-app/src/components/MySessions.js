7b546f68f4961c839d97381254cc06c421b3bf4a
Djouahra Yanis <yanis.djouahra@epitech.eu>
2025-03-13 17:49:25 +0100

    intsall yarn and creat pages

--- my-app/src/components/MySessions.js
+++ my-app/src/components/MySessions.js
@@ -1,8 +1,12 @@
 function MySessions() {
   return (
-    <div>
+    <section className="mysessions">
       <h3>My Sessions </h3>
-    </div>
+      <div className="sessionsCard">
+        <h4>Session 1</h4>
+        <p>Session 1 description</p>
+      </div>
+    </section>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App.js";
-import "./styles/index.css";
+import ReactDOM from "react-dom";
+import App from "../src/components/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
