f44b3ecf118bbc5449017ab2924d0514ceccd0ac
Djouahra Yanis <yanis.djouahra@epitech.eu>
2025-03-14 15:15:59 +0100

    try to use TechniquesList

---
+++ my-app/src/data/TechniquesList.js
@@ -0,0 +1,9 @@
+let Techniques = [
+  {
+    name: "Triangle",
+    description: `Pour exécuter un triangle en JJB :
+    - **Contrôle** : Depuis la garde, passe une jambe par-dessus l’épaule de ton adversaire en gardant un de ses bras à l’intérieur.
+    - **Ajustement** : Croise ta cheville derrière ton genou opposé et tire sa tête vers le bas.
+    - **Verrouillage** : Ajuste l’angle en te décalant de côté, serre les jambes et pousse les hanches pour finaliser l’étranglement.`,
+  },
+];
