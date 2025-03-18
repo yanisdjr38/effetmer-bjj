993acce36892622792ff8de055b45b7726720456
Djouahra Yanis <yanis.djouahra@epitech.eu>
2025-03-18 18:07:01 +0100

    creat sessions card , change css of techniquesCard , start to creat navbar

---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,72 @@
+function NavBar() {
+  return (
+    <nav>
+      <a>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="100"
+          height="100"
+          viewBox="0,0,256,256"
+          style="fill:#2e5077;"
+        >
+          <g
+            fill="#2e5077"
+            fill-rule="nonzero"
+            stroke="none"
+            stroke-width="1"
+            stroke-linecap="butt"
+            stroke-linejoin="miter"
+            stroke-miterlimit="10"
+            stroke-dasharray=""
+            stroke-dashoffset="0"
+            font-family="none"
+            font-weight="none"
+            font-size="none"
+            text-anchor="none"
+            style="mix-blend-mode: normal"
+          >
+            <g transform="scale(10.66667,10.66667)">
+              <path d="M12,2.09961l-11,9.90039h3v9h7v-6h2v6h7v-9h3zM12,4.79102l6,5.40039v0.80859v8h-3v-6h-6v6h-3v-8.80859z"></path>
+            </g>
+          </g>
+        </svg>
+      </a>
+      <a>
+        <svg
+          fill="#2e5077"
+          height="200px"
+          width="200px"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 512 512"
+          xml:space="preserve"
+          stroke="#2e5077"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <path d="M5.224,0v512h501.551V0H5.224z M36.571,31.347h313.469v62.694H36.571V31.347z M245.551,480.653H36.571V125.388h313.469 v250.776h-104.49V480.653z M276.898,407.51h50.977l-50.977,50.977V407.51z M412.735,480.653H299.063l82.324-82.324V125.388h31.347 V480.653z M412.735,94.041h-31.347V31.347h31.347V94.041z M475.429,480.653h-31.347V125.388h31.347V480.653z M475.429,94.041 h-31.347V31.347h31.347V94.041z"></path>{" "}
+              </g>{" "}
+            </g>{" "}
+          </g>
+        </svg>
+      </a>
+      <a></a>
+    </nav>
+  );
+}
+
+export default NavBar;
