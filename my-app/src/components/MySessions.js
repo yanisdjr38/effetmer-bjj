3bad729214922a414beb5484f3cdc13427a4fdbf
Djouahra Yanis <yanis.djouahra@epitech.eu>
2025-03-20 00:47:40 +0100

    add plus button to my sessions

--- my-app/src/components/MySessions.js
+++ my-app/src/components/MySessions.js
@@ -1,12 +1,55 @@
+import React, { useState } from "react";
+import btnPLus from "../assets/images/square-plus.png";
+import btnPLusActive from "../assets/images/square-plusactive.png";
+
 import SessionsCard from "../components/SessionsCard";
 import "../styles/MySessions.css";
 
 function MySessions() {
+  const [icons, setIcons] = useState({
+    plus: btnPLus,
+  });
+
+  const handleImageClick = (key, defaultIcon, activeIcon) => {
+    setIcons((prevIcons) => ({
+      ...prevIcons,
+      [key]: activeIcon,
+    }));
+
+    setTimeout(() => {
+      setIcons((prevIcons) => ({
+        ...prevIcons,
+        [key]: defaultIcon,
+      }));
+    }, 500);
+  };
+
+  const handleMouseEnter = (key, activeIcon) => {
+    setIcons((prevIcons) => ({
+      ...prevIcons,
+      [key]: activeIcon,
+    }));
+  };
+
+  const handleMouseLeave = (key, defaultIcon) => {
+    setIcons((prevIcons) => ({
+      ...prevIcons,
+      [key]: defaultIcon,
+    }));
+  };
+
   return (
     <section className="mysessions">
       <h3>My Sessions</h3>
+      <button
+        onClick={() => handleImageClick("plus", btnPLus, btnPLusActive)}
+        onMouseEnter={() => handleMouseEnter("plus", btnPLusActive)}
+        onMouseLeave={() => handleMouseLeave("plus", btnPLus)}
+      >
+        <img src={icons.plus} alt="plus" /> Add Sessions
+      </button>
       <div className="mysessions-container">
-            <SessionsCard />
+        <SessionsCard />
       </div>
     </section>
   );
