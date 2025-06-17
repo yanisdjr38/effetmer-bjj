6753c030be0379863f6cb490eeb3bcda4a8c3cd3
Djouahra Yanis <yanis.djouahra@epitech.eu>
2025-06-17 15:01:43 +0200

    add hashrooter

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
