3f622122b3942d9a9f37a4231b6d0b5f4a75d1e8
Djouahra Yanis <yanis.djouahra@epitech.eu>
2025-06-17 15:20:24 +0200

    fix routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import TechniquesPage from "./pages/TechniquesPage";
 import TimerPage from "./pages/TimerPage";
@@ -8,15 +8,16 @@ import TrainingPage from "./pages/TrainingPage";
 
 function App() {
   return (
-    <Router>
+    <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/training" element={<TrainingPage />} />
         <Route path="/techniques" element={<TechniquesPage />} />
         <Route path="/timer" element={<TimerPage />} />
       </Routes>
-      <NavBar />
-    </Router>
+      <Navbar />
+    </>
   );
 }
 
