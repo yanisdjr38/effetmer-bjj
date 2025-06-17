6c695ea704a36304e52f75510ee38c3b32911f55
Djouahra Yanis <yanis.djouahra@epitech.eu>
2025-06-17 15:03:16 +0200

    fix index .js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,8 +11,6 @@ root.render(
     <App />
   </HashRouter>
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+// Enregistrement du service worker pour PWA
 serviceWorkerRegistration.register();
